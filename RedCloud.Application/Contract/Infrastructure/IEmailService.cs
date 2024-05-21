8b021b8838de30bc1009edc839c2047fceee77ff
sohamwalke <137477645+sohamwalke@users.noreply.github.com>
2024-05-21 17:08:26 +0530

    Added test classes

---
+++ RedCloud.Application/Contract/Infrastructure/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedCloud.Application.Contract.Infrastructure
+{
+    internal interface IEmailService
+    {
+    }
+}
---
+++ RedCloud.Application/Features/Categories/Queries/Queryclss.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedCloud.Application.Features.Categories.Queries
+{
+    internal class Queryclss
+    {
+    }
+}
