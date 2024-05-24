86a3bc8925011a1d0192a1366730de424e429aef
sohamwalke <137477645+sohamwalke@users.noreply.github.com>
2024-05-24 11:34:57 +0530

    Remove Welcome contain

---
+++ RedCloud.Persistenence/Migrations/20240523150022_RedCloudAddkry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RedCloud.Persistenence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RedCloudAddkry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RedCloudAdmin",
+                table: "ResellerAdmin",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RedCloudAdmin",
+                table: "ResellerAdmin");
+        }
+    }
+}
