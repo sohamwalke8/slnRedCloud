e2a3bdf49cd0875cdc2c6b2c34fa0c95b700d944
Aakash Gupta <guptaaakash333@gmail.com>
2024-05-27 17:34:58 +0530

    Final Login

---
+++ RedCloud.Application/Features/OrganizationsAdmin/Query/AllOrganizationAdminVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedCloud.Domain.Entities
+{
+    public class AllOrganizationAdminVM
+    {
+        public int OrgID { get; set; }
+        public string OrgName { get; set; }
+
+        public string EIN { get; set; }
+
+        public string OrgAdminEmail { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ RedCloud.Application/Features/OrganizationsAdmin/Query/GetAllOrganizationAdminQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using MvcApiCallingService.Models.Responses;
+using RedCloud.Application.Responses;
+using RedCloud.Domain.Entities;
+using RedCloud.ModelVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedCloud.Application.Features.OrganizationsAdmin.Query
+{
+    public class GetAllOrganizationAdminQuery : IRequest<BaseResponse<List<AllOrganizationAdminVM>>>
+    {
+
+
+    }
+}
