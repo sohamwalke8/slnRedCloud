0478e69c9988cbf032919a400f83499dba0113e3
Aakash Gupta <aakash.gupta@neosoftmail.com>
2024-06-20 19:14:26 +0530

    commit

---
+++ RedCloud/Controllers/ResellerAssignCreditController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedCloud.Controllers
+{
+    public class ResellerAssignCreditController : Controller
+    {
+        public IActionResult AssignCreditList()
+        {
+            return View();
+        }
+
+
+        public IActionResult AssignCreditDetailsById(int id)
+        {
+            return View();
+        }
+
+
+    }
+}
