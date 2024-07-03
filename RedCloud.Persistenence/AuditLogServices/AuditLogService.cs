ff660835ef1d0fb77de4a6b27d743ad3218d0a44
ShubhamPawarNeoSoft <shubham.pawar@neosoftmail.com>
2024-07-03 16:43:18 +0530

    commit

---
+++ RedCloud.Persistenence/AuditLogServices/AuditLogService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedCloud.Persistenence.AuditLogServices
+{
+    public interface IAuditLogService
+    {
+        void LogAudit(AuditLog log);
+    }
+
+    public class AuditLogService : IAuditLogService
+    {
+        private readonly YourDbContext _dbContext;
+
+        public AuditLogService(YourDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void LogAudit(AuditLog log)
+        {
+            _dbContext.AuditLogs.Add(log);
+            _dbContext.SaveChanges();
+        }
+    }
+    internal class AuditLogService
+    {
+    }
+}
---
+++ RedCloud/Services/AuditLog.cs
@@ -0,0 +1,12 @@
+﻿namespace RedCloud.Services
+{
+    public class AuditLog
+    {
+        public DateTime DateAndTime { get; set; }
+        public string UserEmail { get; set; }
+        public string UserRole { get; set; }
+        public string OldValues { get; set; }
+        public string NewValues { get; set; }
+    }
+
+}
